2e5bf21193552ac3ed7538e0a93e40d8df160354
Bielgomes <bielgomesdasilva@hotmail.com>
2025-05-10 22:25:04 -0300

    chore(eslint): update ESLint configuration and add plugins for improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,34 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+    { ignores: ['node_modules', 'dist', 'generated'] },
+    {
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.ts'],
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+            'prettier': prettier,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    printWidth: 80,
+                    tabWidth: 4,
+                    singleQuote: true,
+                    trailingComma: 'all',
+                    arrowParens: 'always',
+                    semi: false,
+                },
+            ],
+        }
+    }
+)
